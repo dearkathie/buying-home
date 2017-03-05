1d2a44afcf23bed7b897591838de064c9f8fe1b8
Katja Vegner <katjavegner@gmail.com>
2017-03-05 22:04:57 +0300

    added all needed data from json and make style for big screen

--- js/app.js
+++ js/app.js
@@ -6,27 +6,48 @@ $(document).ready(function() {
             response.json().then((data) => {
                 $("#broker").html(data.Makelaar);
                 $("#phone").html(data.MakelaarTelefoon);
-                var  description = data.VolledigeOmschrijving;
-
-                if (description.length > 500) {
-                   var newText = description.substring(0, 500) + '\.\.\.';
-                  $("#full-description").html(newText);
-                } else {
-                  $("#full-description").html(description);
-                }
-
                 document.getElementById("main-photo").innerHTML = '<img src="' + data.HoofdFoto + '" />';
+
+
+
+                $("#description").html(data.VolledigeOmschrijving);
+
+                // making lenght of property descriptional optional
+                var minimized_elements = $('#description');
+
+                minimized_elements.each(function() {
+                    var t = $(this).text();
+                    if (t.length < 500) return;
+
+                    $(this).html(
+                        t.slice(0, 500) + '<span>... </span><a href="#" class="more">Read more</a>' +
+                        '<span style="display:none;">' + t.slice(500, t.length) + ' <a href="#" class="less">Read less</a></span>'
+                    );
+
+                });
+
+                $('a.more', minimized_elements).click(function(event) {
+                    event.preventDefault();
+                    $(this).hide().prev().hide();
+                    $(this).next().show();
+                });
+
+                $('a.less', minimized_elements).click(function(event) {
+                    event.preventDefault();
+                    $(this).parent().hide().prev().show().prev().show();
+                });
+
+                // create list with property description
                 data.Kenmerken.forEach(function(item) {
-                  var result = item.Kenmerken.map(function(a) {return a.Naam + a.Waarde;});
-                  var kenmerkenData = document.getElementById("kenmerken");
-                  kenmerkenData.innerHTML = kenmerkenData.innerHTML  + item.Titel + result+"<br>";
+                    var kenmerkenName = item.Kenmerken.map(function(a) {
+                        return "<dt>" + a.Naam + "</dt>" + "<dd>" + a.Waarde + "</dd>";
+                    });
 
+                    var nameListDl = document.createElement("dl");
+                    var nameList = document.createTextNode(item.Titel);
+                    nameListDl.appendChild(nameList)
 
-
-
-                    // var stuffData = document.getElementById("stuff");
-                    // stuffData.innerHTML = stuffData.innerHTML + result;
-
+                    $("#list-header").append(nameListDl).append(kenmerkenName)
                 });
             });
         });
